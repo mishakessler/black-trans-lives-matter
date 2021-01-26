96641b04211d62f64a18333c954e9982ef905b15
Misha Kessler <misha.kessler@gmail.com>
2021-01-26 18:41:51 -0500

    Update boilerplate

--- config.js
+++ config.js
@@ -1,35 +1,21 @@
 const config = {
   gatsby: {
     pathPrefix: '/',
-    siteUrl: 'https://hasura.io',
+    siteUrl: 'https://blacktranslivesmatter.co',
     gaTrackingId: null,
     trailingSlash: false,
   },
   header: {
-    logo: 'https://graphql-engine-cdn.hasura.io/learn-hasura/assets/homepage/brand.svg',
-    logoLink: 'https://hasura.io/learn/',
+    logo: '',
+    logoLink: 'https://blacktranslivesmatter.co',
     title:
-      "<a href='https://hasura.io/learn/'><img class='img-responsive' src='https://graphql-engine-cdn.hasura.io/learn-hasura/assets/homepage/learn-logo.svg' alt='Learn logo' /></a>",
-    githubUrl: 'https://github.com/hasura/gatsby-gitbook-boilerplate',
-    helpUrl: '',
+      "<a href='https://blacktranslivesmatter.co'>Actions & Resources for Solidarity</a>",
+    githubUrl: 'https://github.com/mishakessler/black-trans-lives-matter',
+    helpUrl: 'https://github.com/mishakessler/black-trans-lives-matter',
     tweetText: '',
-    social: `<li>
-		    <a href="https://twitter.com/hasurahq" target="_blank" rel="noopener">
-		      <div class="twitterBtn">
-		        <img src='https://graphql-engine-cdn.hasura.io/learn-hasura/assets/homepage/twitter-brands-block.svg' alt={'Twitter'}/>
-		      </div>
-		    </a>
-		  </li>
-			<li>
-		    <a href="https://discordapp.com/invite/hasura" target="_blank" rel="noopener">
-		      <div class="discordBtn">
-		        <img src='https://graphql-engine-cdn.hasura.io/learn-hasura/assets/homepage/discord-brands-block.svg' alt={'Discord'}/>
-		      </div>
-		    </a>
-		  </li>`,
-    links: [{ text: '', link: '' }],
+    links: [{ text: 'Testing', link: '' }],
     search: {
-      enabled: false,
+      enabled: true,
       indexName: '',
       algoliaAppId: process.env.GATSBY_ALGOLIA_APP_ID,
       algoliaSearchKey: process.env.GATSBY_ALGOLIA_SEARCH_KEY,
@@ -38,33 +24,42 @@ const config = {
   },
   sidebar: {
     forcedNavOrder: [
-      '/introduction', // add trailing slash if enabled above
-      '/codeblock',
+      '/',
+      '/perspectives',
+      '/voices',
+      '/events',
+      '/resources',
     ],
     collapsedNav: [
-      '/codeblock', // add trailing slash if enabled above
+      '/',
+      '/perspectives',
+      '/voices',
+      '/events',
+      '/resources',   
     ],
-    links: [{ text: 'Hasura', link: 'https://hasura.io' }],
-    frontline: false,
+    links: [
+      { text: 'Contribute', link: 'https://github.com/mishakessler/black-trans-lives-matter' },
+      { text: 'Give Feedback', link: 'mailto:readytoactfeedback@gmail.com' }],
+    frontline: true,
     ignoreIndex: true,
     title:
-      "<a href='https://hasura.io/learn/'>graphql </a><div class='greenCircle'></div><a href='https://hasura.io/learn/graphql/react/introduction/'>react</a>",
+      "<div class='greenCircle'></div>honor black trans lives<div class='greenCircle'></div>",
   },
   siteMetadata: {
-    title: 'Gatsby Gitbook Boilerplate | Hasura',
-    description: 'Documentation built with mdx. Powering hasura.io/learn ',
+    title: '#BlackTransLivesMatter',
+    description: 'Actions & Resources for Solidarity',
     ogImage: null,
-    docsLocation: 'https://github.com/hasura/gatsby-gitbook-boilerplate/tree/master/content',
-    favicon: 'https://graphql-engine-cdn.hasura.io/img/hasura_icon_black.svg',
+    docsLocation: 'https://github.com/mishakessler/black-trans-lives-matter/tree/master/content',
+    favicon: {src: 'src/pwa-512.png'},
   },
   pwa: {
-    enabled: false, // disabling this will also remove the existing service worker.
+    enabled: true, // disabling this will also remove the existing service worker.
     manifest: {
-      name: 'Gatsby Gitbook Starter',
-      short_name: 'GitbookStarter',
+      name: '#BlackTransLivesMatter',
+      short_name: '#BlackTransLivesMatter',
       start_url: '/',
-      background_color: '#6b37bf',
-      theme_color: '#6b37bf',
+      background_color: '#ffffff',
+      theme_color: '#4bb2c6',
       display: 'standalone',
       crossOrigin: 'use-credentials',
       icons: [
